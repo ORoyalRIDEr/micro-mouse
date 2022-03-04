5136eedb73573bb63a5ad099b68ea0c1a4e815b6
Jakob Dommaschk <dommaschk@campus.tu-berlin.de>
2022-03-04 13:27:05 +0100

    SLAM algorithm tested and adjusted.

--- Test/src/test_slam.c
+++ Test/src/test_slam.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
+#include "../../Core/Inc/Lib/cmath.h"
 #include "../../Core/Inc/Ecl/state_estimator.h"
-
-void slam(int32_t dist[], uint32_t maze_pos[]);
+#include "../../Core/Inc/Ecl/orientation_ctrl.h"
 
 extern int32_t est_pos[]; // um
 extern int32_t est_V;          // um/s
 extern int32_t est_Psi; // 1000 rad, -pi ... pi
 
+int32_t map[][8] = {
+      {1, 1, 1, 1, 1, 1, 1, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+      {1, 1, 1, 1, 1, 1, 1, 0}
+};
+
 void test_slam()
 {
     init_maze();
+    est_Psi = 1571*1000;
 
-    est_Psi = -1571;
-    est_pos[0] =  50 * 1000 + 5*200*1000;
-    est_pos[1] = 100 * 1000 + 5*200*1000;
+    est_pos[0] = (200+10-45)*1000;
+    est_pos[1] = (200+10-60)*1000;
+    //est_pos[1] = 100 * 1000 + 5*200*1000;
 
     print_maze();
 
     //uint32_t dist[] = {40000+1000, 90000-1000, 40000+3000, 140000+200000-3000};
-    uint32_t dist[] = {40000, 0, 55000, 0x0FFFFFFF};
-    uint32_t maze_pos[2];
+    //uint32_t dist[] = {55000, 0, 400000, 55000};
+    uint32_t dist[] = {400000, 0, 180000, 55000};
 
-    for (int i=0; i<20; i++)
-        slam(dist, maze_pos);
-        print_maze();
+    for (int i=0; i<20; i++) {
+      slam(dist);
+      //printf("Pos: (%i, %i)\n", est_pos[0], est_pos[1]);
+    }
+    print_maze();
 }
